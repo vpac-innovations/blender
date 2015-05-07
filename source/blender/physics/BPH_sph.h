e26364144232cf8eae61785dfe229cec92b493ee
Sean Loh <sean.loh@vpac-innovations.com.au>
2015-05-07 14:15:54 +1000

    Changed coalescing methodology.
    Particles array no longer re-sized, when two particles are merged
    one has it's dietime set to current frame effectively removing it
    from the computation.

--- source/blender/physics/BPH_sph.h
+++ source/blender/physics/BPH_sph.h
@@ -98,7 +98,7 @@ typedef struct SPHData {
 } SPHData;
 
 /* General SPH functions */
-void BPH_sph_unsplit_particle(ParticleSimulationData *sim, int index);
+void BPH_sph_unsplit_particle(ParticleSimulationData *sim, int index, float cfra);
 void BPH_sph_split_particle(ParticleSimulationData *sim, int index, float cfra);
 
 /* DDR SPH */
